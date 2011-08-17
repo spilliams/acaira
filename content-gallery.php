e3a21d432d7c6fea638f0465688dc469b7ea79be
Spencer Williams <s@spencerenglish.com>
2011-08-17 12:32:32 -0400

    category view for gallery posts and standard posts

--- content-gallery.php
+++ content-gallery.php
@@ -1,1 +1,7 @@
-content-gallery
+<?php /* display a gallery among other posts */ ?>
+<!-- begin content-gallery -->
+<a href='<?php the_permalink(); ?>'><div class='gallery-thumb'>
+  <?php echo get_the_post_thumbnail($post->ID, 'medium'); ?>
+  <span class='gallery-title' style='display:none'><?php the_title(); ?><br><?php the_date(); ?></span>
+</div></a>
+<!-- end content-gallery -->
--- content.php
+++ content.php
@@ -1,1 +1,11 @@
-content
+<!-- begin content -->
+  <a href='<?php the_permalink(); ?>'><div class='gallery-thumb'>
+    <span class='gallery-content'>
+      <?php
+      $t = get_the_content();
+      echo $t[0];
+      ?>
+    </span>
+    <span class='gallery-title' style='display:none'><?php the_title(); ?><br><?php the_date(); ?></span>
+  </div></a>
+<!-- end content -->
