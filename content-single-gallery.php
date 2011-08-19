46a3455355f6f29a48fcf18b96eaf3137edce2fb
Spencer Williams <s@spencerenglish.com>
2011-08-18 22:34:30 -0400

    gallery content

--- content-single-gallery.php
+++ content-single-gallery.php
@@ -1,1 +1,55 @@
-content-single-gallery
+<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="entry-header">
+		<h1 class="entry-title"><?php the_title(); ?></h1>
+	</div><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'acaira' ) . '</span>', 'after' => '</div>' ) ); ?>
+	</div><!-- .entry-content -->
+
+	<div class="entry-meta">
+		<?php
+			/* translators: used between list items, there is a space after the comma */
+			$categories_list = get_the_category_list( __( ', ', 'acaira' ) );
+
+			/* translators: used between list items, there is a space after the comma */
+			$tag_list = get_the_tag_list( '', __( ', ', 'acaira' ) );
+			if ( '' != $tag_list ) {
+				$utility_text = __( 'This entry was posted in %1$s and tagged %2$s by <a href="%6$s">%5$s</a>. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'acaira' );
+			} elseif ( '' != $categories_list ) {
+				$utility_text = __( 'This entry was posted in %1$s by <a href="%6$s">%5$s</a>. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'acaira' );
+			} else {
+				$utility_text = __( 'This entry was posted by <a href="%6$s">%5$s</a>. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'acaira' );
+			}
+
+			printf(
+				$utility_text,
+				$categories_list,
+				$tag_list,
+				esc_url( get_permalink() ),
+				the_title_attribute( 'echo=0' ),
+				get_the_author(),
+				esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) )
+			);
+		?>
+		<?php edit_post_link( __( 'Edit', 'acaira' ), '<span class="edit-link">', '</span>' ); ?>
+
+		<?php if ( get_the_author_meta( 'description' ) && is_multi_author() ) : // If a user has filled out their description and this is a multi-author blog, show a bio on their entries ?>
+		<div id="author-info">
+			<div id="author-avatar">
+				<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'acaira_author_bio_avatar_size', 68 ) ); ?>
+			</div><!-- #author-avatar -->
+			<div id="author-description">
+				<h2><?php printf( esc_attr__( 'About %s', 'acaira' ), get_the_author() ); ?></h2>
+				<?php the_author_meta( 'description' ); ?>
+				<div id="author-link">
+					<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
+						<?php printf( __( 'View all posts by %s <span class="meta-nav">&rarr;</span>', 'acaira' ), get_the_author() ); ?>
+					</a>
+				</div><!-- #author-link	-->
+			</div><!-- #author-description -->
+		</div><!-- #entry-author-info -->
+		<?php endif; ?>
+	</div><!-- .entry-meta -->
+</div><!-- #post-<?php the_ID(); ?> -->
