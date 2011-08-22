61916fecd981a0cbd3d7ef9ccb21dffce25d9eea
Spencer Williams <s@spencerenglish.com>
2011-08-21 21:56:48 -0400

    home images, styles

--- content-home.php
+++ content-home.php
@@ -1,27 +1,11 @@
 <div id='cUp'>
   <script type='text/javascript'>upIds=['h1','h2','h3','h4','h5'];upId='h1';</script>
   <!-- link these when they're finally up -->
-  <div id='h1' class='up'><img src='/images/home/h1.jpg'></div>
-  <div id='h2' class='up hidden'><img src='/images/home/h2.jpg'</div>
-  <div id='h3' class='up hidden'><img src='/images/home/h3.jpg'</div>
-  <div id='h4' class='up hidden'><img src='/images/home/h4.jpg'</div>
-  <div id='h5' class='up hidden'><img src='/images/home/h5.jpg'</div>
-    <?php
-    $args = array( 'numberposts' => 9, 'orderby' => 'rand' );
-    $rand_posts = get_posts( $args );
-    $first = true;
-    foreach( $rand_posts as $post ) :
-      $image_full_tag = get_the_post_thumbnail($post->ID,'Homepage Slideshow'); /* 1023 x 682 */
-      $image_id = get_post_thumbnail_id($post->ID);
-      ?>
-      <div id='<?php echo $image_id; ?>' class='up<?php if ( !$first ) : ?> hidden<?php endif; ?>'>
-        <script type='text/javascript'>upIds.push(<?php echo $image_id; ?>)</script>
-      	<a href='<?php echo the_permalink(); ?>#<?php echo get_post_thumbnail_id(); ?>'>
-      	  <?php echo $image_full_tag; ?>
-      	</a>
-      </div><!-- up -->
-    	<?php $first = false; ?>
-    <?php endforeach; ?>
+  <div id='h1' class='up'><img src="<?php bloginfo('stylesheet_directory'); ?>/images/home/h1.jpg"></div>
+  <div id='h2' class='up hidden'><img src='<?php bloginfo('stylesheet_directory'); ?>/images/home/h2.jpg'></div>
+  <div id='h3' class='up hidden'><img src='<?php bloginfo('stylesheet_directory'); ?>/images/home/h3.jpg'></div>
+  <div id='h4' class='up hidden'><img src='<?php bloginfo('stylesheet_directory'); ?>/images/home/h4.jpg'></div>
+  <div id='h5' class='up hidden'><img src='<?php bloginfo('stylesheet_directory'); ?>/images/home/h5.jpg'></div>
 </div>
 <script type='text/javascript'>
   function slideshowGo() {
